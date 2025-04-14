f9e37a8f94af004b7a4fdf5d9f3a82bcaa25ce92
surafel <surafeladmas721@gmail.com>
2025-04-14 10:54:59 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: process.env.NODE_ENV === 'production' ? https://jsforum.vercel.app' : ['http://localhost:5173', 'http://localhost:3000'],
+    origin: process.env.NODE_ENV === 'production' ? 'https://jsforum.vercel.app' : ['http://localhost:5173', 'http://localhost:3000'],
     credentials: true
 }));
 
