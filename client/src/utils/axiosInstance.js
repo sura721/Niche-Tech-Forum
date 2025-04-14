b17cc899be7427e47b0f321f3f1a7b0560eeb11a
surafel <surafeladmas721@gmail.com>
2025-04-14 11:09:33 +0300

    Update axiosInstance.js url

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-  baseURL:import.meta.env.VITE_API_URL|| "https://niche-tech-forum.onrender.com",
+  baseURL:import.meta.env.VITE_API_URL|| "https://niche-tech-forum.onrender.com/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
